482add84a0cb467f76f36820712df570f3a99448
JustMause <glebromanenko666@gmail.com>
2025-10-30 11:29:20 +0300

    Update arrays.py

--- src/lab2/arrays.py
+++ src/lab2/arrays.py
@@ -1,19 +1,26 @@
-a = [3, -1, 5, 5, 0]
-b = [3, 1, 2, 1, 3]
-c = [[1, 2], [3, 4]]
-mat2 = []
+a = [[1, 2], [3, 4]]
+b = [[1, 2], (3, 4, 5)]
+c = [[1], [], [2, 3]]
+d = [[1, 2], "ab"]
 def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
+    if not nums:
+        raise ValueError()
     return (min(nums), max(nums))
 
 def unique_sorted(nums: list[float | int]) -> tuple[float | int, float | int]:
     return (sorted(set(nums)))
 
 def flatten(mat: list[list | tuple]) -> list:
+    mat2 = []
     for el in mat:
+        if not isinstance(el, (list, tuple)):
+            raise TypeError()
+
         mat2.extend(el)
     return(mat2)
 
 
-print(min_max(a))
-print(unique_sorted(b))
+print(flatten(a))
+print(flatten(b))
 print(flatten(c))
+print(flatten(d))
